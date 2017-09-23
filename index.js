6f6d6ef0e33396e14cb5bd69f546eff85c0b3eed
Geoffrey Ostosh <ostoshg@gmail.com>
2017-09-23 08:56:13 -0700

    add chunked file reader proto implementation

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function loadFile(file, cb) {
+  var chunkSize = 1024;
+  var index = 0;
+  var fileReader = new FileReader();
+  
+  fileReader.onload = function(e) {
+
+    cb(e.target.result);
+    index += chunkSize;
+    
+    if(index < file.size)//continue
+   	 readChunk();  
+  };
+
+  fileReader.onerror = function() {
+    cb(null);
+  };
+  
+  readChunk();//start 
+
+  function readChunk() {
+      var nextChunk = file.slice(index, index + chunkSize);
+      fileReader.readAsBinaryString(nextChunk);
+  }
+}
